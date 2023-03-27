7a816508c7c3b83533b3fa6370e5e184629e9d95
Jess Berler <jessdev83@gmail.com>
2023-03-27 10:35:59 -0500

    initial commit, pusher data working

--- CraveRetailPusherTakeHome/ViewController.swift
+++ CraveRetailPusherTakeHome/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import PusherSwift
+import Foundation
 
 class ViewController: UIViewController {
-
+  
+  var pusher: Pusher! = nil
+  let decoder = JSONDecoder()
+  
+  @IBAction func reconnect(_ sender: AnyObject) {
+    pusher.connect()
+  }
+  
+  @IBAction func disconnect(_ sender: AnyObject) {
+    pusher.disconnect()
+  }
+  
+  func calculateSize(_ data: AnyObject) -> String {
+    let bytes = data.count
+    let formatter = ByteCountFormatter()
+    formatter.allowedUnits = [.useBytes]
+    formatter.countStyle = .file
+    let sizeString = formatter.string(fromByteCount: Int64(bytes ?? 0))
+    return sizeString
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    let options = PusherClientOptions(
+      host: .cluster("us3")
+    )
+    pusher = Pusher(key: "5d3657875c763e1b0282", options: options)
+    pusher.connect()
+    
+    let sampleChannel = pusher.subscribe(channelName: "sample")
+    let _ = sampleChannel.bind(eventName: "sampleMessage", callback: { (data: Any?) -> Void in
+      if let sampleData = data as? [String : AnyObject] {
+        let message = sampleData["message"] as! String
+        let title = sampleData["title"] as! String
+        let messageSize = self.calculateSize(data as AnyObject)
+        print("Title: \(title), Message: \(message), Size: \(messageSize)")
+      }
+    })
   }
-
-
 }
-
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'CraveRetailPusherTakeHome' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'PusherSwift'
+
+  # Pods for CraveRetailPusherTakeHome
+
+  target 'CraveRetailPusherTakeHomeTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CraveRetailPusherTakeHomeUITests' do
+    # Pods for testing
+  end
+
+end
